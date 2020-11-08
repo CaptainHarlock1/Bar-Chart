68bf02eb422a686dd79b7fbc894abda2d1e45a73
CaptainHarlock <anekav@gmail.com>
2020-11-08 15:54:40 +0200

    alsmost there

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-let dataset = [];
+let dataset = "";
 let datasetobj = [];
 const w = 550;
 const h = 450;
 const padding = 30;
 
 const xScale = d3.scaleLinear()
-                .domain([0, d3.max(dataset, (d)=> d[0])])
+                .domain([d3.min(dataset, (d)=> newDate(d[0])), d3.max(dataset, (d)=> newDate(d[0]))])
                 .range([padding, w-padding]);
 
 const yScale = d3.scaleLinear()
@@ -16,16 +16,28 @@ const yScaleaxis = d3.scaleLinear()
                 .domain([0, d3.max(dataset, (d)=>d[1])])
                 .range([h-padding, padding]);
 
-const title = d3.select("body")
-                .append("h1")
-                .text("GDP Bar Chart")
-                .attr("id", "title")
-                .attr("class", "title");
+// const title = d3.select("body")
+//                 .append("h1")
+//                 .text("GDP Bar Chart")
+//                 .attr("id", "title")
+//                 .attr("class", "title");
 const canvas = d3.select("body")
                 .append("svg")
                 .attr("width", w)
                 .attr("height", h);
-                
+      
+          canvas.append("text")
+                .attr("x", w/2)             
+                .attr("y", 20)
+                .text("GDP Bar Chart")
+                .attr("anchor", "middle")
+                .attr("class", "title")
+                .attr("text-anchor", "middle")  
+            //     .style("font-size", "21px") 
+            //     .style("color", "white");  
+                  
+                      
+             
 
 
 
@@ -47,32 +59,28 @@ canvas.selectAll("rect")
       .data(dataset)
       .enter()
       .append("rect")
-      .attr("x", (d)=> xScale(d[0])-(10/2))
+      .attr("x", (d, i)=> i*50)
       .attr("y", (d)=> (h-yScale(d[1])))
       .attr("width", 10)
       .attr("height", (d)=> yScale(d[1])-padding)
       .attr("class", "bar");
 
-   body.append("h2")
-      .text("It working");
-
-
-
-
-// const req = new XMLHttpRequest();
-// req.open("GET", 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json', true );
-// req.send();
-// req.onload = function(){
-//      const json = JSON.parse(req.responseText);
+ 
+const req = new XMLHttpRequest();
+req.open("GET", 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json', true );
+req.send();
+req.onload = function(){
+     const json = JSON.parse(req.responseText);
      
-//      datasetobj.push(json.data);
-//      dataset = datasetobj[0].map(d=>d);
-//      console.log(dataset);
-//      console.log(typeof(dataset));
+     datasetobj.push(json.data);
+     dataset = datasetobj[0].map(d=>d);
+     console.log(dataset);
+     console.log(typeof(dataset));
+     console.log(texti);
 
 // //      document.getElementById('test').innerText = dataset;
 
-// };
+};
 
 
 
